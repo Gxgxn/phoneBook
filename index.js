7bd5d59957b1603fbd93809e41df2e9388d80514
Gxgxn <gdsinghb99@gmail.com>
2023-04-14 23:02:22 -0300

    added get routes

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
 
 const persons = [
   {
@@ -25,3 +29,25 @@ const persons = [
     number: "39-23-6423122",
   },
 ];
+app.get("/", (req, res) => {
+  res.status(200).json(persons);
+});
+app.get("/info", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  const date = new Date();
+  res
+    .status(200)
+    .end(
+      `<p>Phone Book has Info for ${persons.length} people</p><p>${date}</p>`
+    );
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const personId = Number(req.params.id);
+  const person = persons.find((person) => person.id === personId);
+  if (!person) {
+    return res.status(404).end();
+  }
+  console.log(person);
+  res.status(202).json(person);
+});
